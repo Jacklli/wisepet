179297edb77a302be4aafcae7d35cc7a55d5c23f
Jacklli <yinlong.lee@hotmail.com>
2016-02-07 11:47:28 +0800

    clean wasted code.

--- boot.asm
+++ boot.asm
@@ -1,11 +1,11 @@
-	org	07c00h			; 告诉编译器程序加载到7c00处
+	org	7c00h			; 告诉编译器程序加载到7c00处
 	call	DispStr			; 调用显示字符串例程
 	jmp	$			; 无限循环
 DispStr:
 	mov	ax, BootMessage
 	mov	bp, ax			; ES:BP = 串地址
 	mov	cx, 13			; CX = 串长度
-	mov	ax, 01301h		; AH = 13,  AL = 01h
+	mov	ax, 1301h		; AH = 13,  AL = 01h
 	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 0
 	int	10h			; 10h 号中断
